89aa8c927bf10216fcd1b9ac5d7467f9948909d4
root <root@GramSlam>
2024-10-02 16:46:31 -0700

    refactored chatroom management

--- server.py
+++ server.py
@@ -1,29 +1,39 @@
-import socket 
-import select 
-import sys 
+import socket
+import select
+import sys
 from _thread import *
 
+class ChatRoom:
+    next_room_num = 0
+    def __init__(self, conn):
+        self.room_num = self.next_room_num
+        self.next_room_num += 1
+        self.clients = [conn]
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
+    def add_client(self, conn):
+        self.clients.append(conn)
 
-if len(sys.argv) != 3: 
+    def remove_client(self, conn):
+        self.clients.remove(conn)
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+if len(sys.argv) != 3:
     print("Error: must give script, IP address, port number")
     exit()
-    
+
 IP_address = str(sys.argv[1])
 
 Port = int(sys.argv[2])
 
-server.bind((IP_address, Port)) 
+server.bind((IP_address, Port))
 
-server.listen(100) 
- 
-chatrooms = {}
-chat_room_num = 1
+server.listen(100)
+
+chatrooms = []
 
 def client_thread(conn, addr):
-    global chat_room_num
     conn.send("Welcome! If you would like to disconnect, press Enter (no input)\n".encode())
     conn.send("Please enter your username: ".encode())
     username = conn.recv(2048).decode().strip()
@@ -38,51 +48,51 @@ def client_thread(conn, addr):
             action = prompt_actions(conn)
             selected_room = 0
             if action == '1':
-                selected_room = chat_room_num
-
-                chatrooms[selected_room] = [conn]
-                chat_room_num += 1
+                selected_room = ChatRoom(conn)
+                chatrooms.append(selected_room)
 
             elif action == '2':
-                selected_room = select_chat_room(conn)
-                if selected_room is None:
+                selected_room_num = select_chat_room(conn)
+                if selected_room_num is None:
                     continue
-                chatrooms[selected_room].append(conn)
+                chatrooms[selected_room_num].add_client(conn)
+                selected_room = chatrooms[selected_room_num]
             else:
                 close_connection(conn, addr)
                 return
 
             broadcast_message(selected_room, f"{username} has joined.")
             await_messages(selected_room, username, conn)
-            print(f"{username} left chat room {selected_room}")
+            print(f"{username} left chat room {selected_room.room_num}")
         except:
+            # close_connection(conn, addr)
             continue
-            
+
 def close_connection(conn, addr):
     print(f"Client at {addr[0]} closed connection")
     conn.close()
 
-def await_messages(room_num, username, conn):
+def await_messages(room, username, conn):
     conn.send("To leave the chat room, press Enter (no input)\n".encode())
     while True:
         try:
             message = conn.recv(2048).decode().strip()
             if message and message != "":
-                broadcast_message(room_num, f"{username}: {message}")
+                broadcast_message(room, f"{username}: {message}")
             else:
-                chatrooms[room_num].remove(conn)
-                broadcast_message(room_num, f"{username} has left the chat.")
+                chatrooms[room.room_num].remove_client(conn)
+                print(chatrooms[room.room_num].__dict__)
+                broadcast_message(room, f"{username} has left the chat.")
                 break
         except:
             break
 
-def broadcast_message(room_num, message):
-    for conn in chatrooms[room_num]:
+def broadcast_message(room, message):
+    for conn in room.clients:
         try:
             conn.send(message.encode())
-        except: 
-            close_connection(conn)
-            chatrooms[room_num].remove(conn) 
+        except:
+            room.remove_client(conn)
 
 def prompt_actions(conn):
     user_input = " "
@@ -95,24 +105,26 @@ def prompt_actions(conn):
         if user_input not in ('1', '2', '3'):
             conn.send("ERROR: Input other than '1', '2', or '3' received. Try again:".encode())
     return user_input
-    
+
 def select_chat_room(conn):
-    if chat_room_num == 1:
+    if len(chatrooms) == 0:
         conn.send("No chat rooms available!".encode())
         return None
-    room_selection = 0
-    while room_selection < 1 or room_selection > chat_room_num - 1:
+    room_selection = -1
+    while room_selection < 0 or room_selection > len(chatrooms) - 1:
         conn.send("Select a chatroom to join:\n".encode())
-        for num in range(1, chat_room_num):
-            conn.send(f"Chatroom {num}\n".encode())
+        for chatroom in chatrooms:
+            conn.send(f"Chatroom {chatroom.room_num}\n".encode())
         conn.send("Join chat room #: ".encode())
-        room_selection = conn.recv(2048).decode()
+        room_selection = conn.recv(2048).decode().strip()
+        print(room_selection)
         try:
             room_selection = int(room_selection)
+            print(room_selection)
         except ValueError:
             conn.send("ERROR: Non integer response detected.\n")
-            room_selection = 0
-        if room_selection < 1 or room_selection > chat_room_num - 1:
+            room_selection = -1
+        if room_selection < 0 or room_selection > len(chatrooms) - 1:
             conn.send("Try Again, please enter a valid room number\n")
     return room_selection
 
